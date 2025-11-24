67b1660c67b687b438537be1071d4b5fded8b019
AkashSabu-17 <akashsabu14@gmail.com>
2025-11-24 11:24:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/second")
+def second_page():
+    return render_template("second.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
