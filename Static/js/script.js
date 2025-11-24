8d92f1f22acba9912a8073127e85a471efd21a2d
AkashSabu-17 <akashsabu14@gmail.com>
2025-11-24 11:45:31 +0530

    Delete Static directory

--- Static/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const btn = document.getElementById("btn");
-  const msg = document.getElementById("msg");
-
-  btn.addEventListener("click", function () {
-    msg.textContent = "Button clicked! Current time: " + new Date().toLocaleString();
-  });
-});
